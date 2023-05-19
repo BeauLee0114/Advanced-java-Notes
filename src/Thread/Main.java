ed4a00d8244fb994d45fd053c434c37c0e19f89f
lb <714268782@qq.com>
2023-05-19 16:48:18 +0800

    Update 线程.md file

---
+++ src/Thread/Main.java
@@ -0,0 +1,8 @@
+package Thread;/**
+* Created by IntelliJ IDEA.
+* @Author : 李博
+* @Description
+* @create 2023/5/19 14:16
+* @Modified By
+*/public class Main {
+}
---
+++ src/Thread/ThreadRunnable.java
@@ -0,0 +1,7 @@
+package Thread;
+public class Thread02 implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Hello from RUnnable");
+    }
+}
